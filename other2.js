0e71df645c0fb9ac212693f11505d0fc382747d4
githendumukiri <Barakaboston12>
2022-02-24 14:06:20 -0500

    logic for forecast data

--- other2.js
+++ other2.js
@@ -1,14 +1,16 @@
 export function outsideMethod2() {
 
     const location = 'london'
-    
+
     async function fetchWeatherJSON() {
         const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e5fa685f438648b390a181343220302&q=${location}&days=1&aqi=no&alerts=no`);
         console.log(response);
         return await response.json();
     }
+
     fetchWeatherJSON().then(obj => {
         // current weather data
+        console.log(obj)
         const cloud = obj.current.cloud;
         const condition_text = obj.current.condition.text;
         const feelsLike_f = obj.current.feelslike_f;
@@ -37,29 +39,32 @@ export function outsideMethod2() {
 
     // forecast data
     void function setForecastDay(day) {
-        //astro
-        const moon_illumination = obj.forecast.forecastday[day].astro.day0_moon_illumination;
-        const moon_phase = obj.forecast.forecastday[day].astro.day0_moon_phase;
-        const moonrise = obj.forecast.forecastday[day].astro.day0_moonrise;
-        const moonset = obj.forecast.forecastday[day].astro.day0_moonset;
-        const sunrise = obj.forecast.forecastday[day].astro.day0_sunrise;
-        const sunset = obj.forecast.forecastday[day].astro.day0_sunset;
 
-        const date = obj.forecast.forecastday[day].date;
+        fetchWeatherJSON().then(obj => {
+            //astro
+            const moon_illumination = obj.forecast.forecastday[day].astro.day0_moon_illumination;
+            const moon_phase = obj.forecast.forecastday[day].astro.day0_moon_phase;
+            const moonrise = obj.forecast.forecastday[day].astro.day0_moonrise;
+            const moonset = obj.forecast.forecastday[day].astro.day0_moonset;
+            const sunrise = obj.forecast.forecastday[day].astro.day0_sunrise;
+            const sunset = obj.forecast.forecastday[day].astro.day0_sunset;
 
-        //weather
-        const avgtemp_f = obj.forecast.forecastday[day].day.avgtemp_f;
-        const forecast_condition_text = obj.forecast.forecastday[day].day.condition.text;
-        const daily_chance_of_rain = obj.forecast.forecastday[day].day.daily_chance_of_rain;
-        const daily_chance_of_snow = obj.forecast.forecastday[day].day.daily_chance_of_snow;
-        const daily_will_it_rain = obj.forecast.forecastday[day].day.daily_will_it_rain;
-        const daily_will_it_snow = obj.forecast.forecastday[day].day.daily_will_it_snow;
-        const maxtemp_f = obj.forecast.forecastday[day].day.maxtemp_f;
-        const maxwind_mph = obj.forecast.forecastday[day].day.maxwind_mph;
-        const mintemp_c = obj.forecast.forecastday[day].day.mintemp_c;
-        const mintemp_f = obj.forecast.forecastday[day].day.mintemp_f;
-        const totalprecip_in = obj.forecast.forecastday[day].day.totalprecip_in;
-        const uv = obj.forecast.forecastday[day].day.uv;
+            const date = obj.forecast.forecastday[day].date;
+
+            //weather
+            const avgtemp_f = obj.forecast.forecastday[day].day.avgtemp_f;
+            const forecast_condition_text = obj.forecast.forecastday[day].day.condition.text;
+            const daily_chance_of_rain = obj.forecast.forecastday[day].day.daily_chance_of_rain;
+            const daily_chance_of_snow = obj.forecast.forecastday[day].day.daily_chance_of_snow;
+            const daily_will_it_rain = obj.forecast.forecastday[day].day.daily_will_it_rain;
+            const daily_will_it_snow = obj.forecast.forecastday[day].day.daily_will_it_snow;
+            const maxtemp_f = obj.forecast.forecastday[day].day.maxtemp_f;
+            const maxwind_mph = obj.forecast.forecastday[day].day.maxwind_mph;
+            const mintemp_c = obj.forecast.forecastday[day].day.mintemp_c;
+            const mintemp_f = obj.forecast.forecastday[day].day.mintemp_f;
+            const totalprecip_in = obj.forecast.forecastday[day].day.totalprecip_in;
+            const uv = obj.forecast.forecastday[day].day.uv;
+        });
     }
 
 
