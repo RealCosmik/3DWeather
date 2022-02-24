57543c3eddf648e5ef8f42c746492dfcbfbb2acd
githendumukiri <Barakaboston12>
2022-02-24 12:33:05 -0500

    added setforecastday() to assign parse weather data corresponding to a given day

--- other2.js
+++ other2.js
@@ -1,4 +1,3 @@
-
 export function outsideMethod2() {
 
     let obj;
@@ -18,11 +17,12 @@ export function outsideMethod2() {
     const last_updated = obj.current.last_updated;
     const precip_in = obj.current.precip_in;
     const precip_mm = obj.current.precip_mm;
-    const temp_f  = obj.current.temp_f;
-    const uv  = obj.current.uv;
+    const temp_f = obj.current.temp_f;
+    const uv = obj.current.uv;
     const wind_degree = obj.current.wind_degree;
     const wind_dir = obj.current.wind_dir;
     const wind_mph = obj.current.wind_mph;
+
     // location data
     const country = obj.current.location.country;
     const lat = obj.current.location.lat;
@@ -33,6 +33,32 @@ export function outsideMethod2() {
     const region = obj.current.location.region;
     const tz_id = obj.current.location.tz_id;
 
+    // forecast data
+    void function setForecastDay(day) {
+        //astro
+        const moon_illumination = obj.forecast.forecastday[day].astro.day0_moon_illumination;
+        const moon_phase = obj.forecast.forecastday[day].astro.day0_moon_phase;
+        const moonrise = obj.forecast.forecastday[day].astro.day0_moonrise;
+        const moonset = obj.forecast.forecastday[day].astro.day0_moonset;
+        const sunrise = obj.forecast.forecastday[day].astro.day0_sunrise;
+        const sunset = obj.forecast.forecastday[day].astro.day0_sunset;
+
+        const date = obj.forecast.forecastday[day].date;
+
+        //weather
+        const avgtemp_f = obj.forecast.forecastday[day].day.avgtemp_f;
+        const forecast_conditon_text = obj.forecast.forecastday[day].day.condition.text;
+        const daily_chance_of_rain = obj.forecast.forecastday[day].day.daily_chance_of_rain;
+        const daily_chance_of_snow = obj.forecast.forecastday[day].day.daily_chance_of_snow;
+        const daily_will_it_rain = obj.forecast.forecastday[day].day.daily_will_it_rain;
+        const daily_will_it_snow = obj.forecast.forecastday[day].day.daily_will_it_snow;
+        const maxtemp_f = obj.forecast.forecastday[day].day.maxtemp_f;
+        const maxwind_mph = obj.forecast.forecastday[day].day.maxwind_mph;
+        const mintemp_c = obj.forecast.forecastday[day].day.mintemp_c;
+        const mintemp_f = obj.forecast.forecastday[day].day.mintemp_f;
+        const totalprecip_in = obj.forecast.forecastday[day].day.totalprecip_in;
+        const uv = obj.forecast.forecastday[day].day.uv;
+    }
 
 
 }
