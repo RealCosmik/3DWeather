33115dfd5bd681cd7c891cc38ea88e9e1801b719
githendumukiri <Barakaboston12>
2022-02-24 11:55:57 -0500

    current & location variables parsed

--- other2.js
+++ other2.js
@@ -8,17 +8,30 @@ export function outsideMethod2() {
         .then(data => obj = data)
         .then(data => console.log(data));
 
+    // current weather data
     const cloud = obj.current.cloud;
-    const conditonText = obj.current.conditon.text;
-    const feelsLikeF = obj.current.feelslike_f
-    const humidity = obj.current.humidity
+    const conditon_text = obj.current.conditon.text;
+    const feelsLike_f = obj.current.feelslike_f;
+    const humidity = obj.current.humidity;
     // 1 = yes, 0 = no
-    const isDay = obj.current.is_day
-    const last_updated = obj.current.last_updated
-
-
-
-
+    const is_day = obj.current.is_day;
+    const last_updated = obj.current.last_updated;
+    const precip_in = obj.current.precip_in;
+    const precip_mm = obj.current.precip_mm;
+    const temp_f  = obj.current.temp_f;
+    const uv  = obj.current.uv;
+    const wind_degree = obj.current.wind_degree;
+    const wind_dir = obj.current.wind_dir;
+    const wind_mph = obj.current.wind_mph;
+    // location data
+    const country = obj.current.location.country;
+    const lat = obj.current.location.lat;
+    const localtime = obj.current.location.localtime;
+    const localtime_epoch = obj.current.location.localtime_epoch;
+    const lon = obj.current.location.lon;
+    const name = obj.current.location.name;
+    const region = obj.current.location.region;
+    const tz_id = obj.current.location.tz_id;
 
 
 
