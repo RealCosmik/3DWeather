6ab9e96c95e1c9599e6cd8af781a38c6d7f32106
githendumukiri <Barakaboston12>
2022-02-24 13:48:26 -0500

    Syntax error & fetch update

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 import './style.css'
 import * as THREE from 'three'
 import * as OTHER from "./other.js"
+import {outsideMethod2} from "./other2";
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -18,6 +19,8 @@ scene.add(cube);
 
 
 const location = 'london';
+
+outsideMethod2()
 
 fetch(`http://api.weatherapi.com/v1/forecast.json?key=e5fa685f438648b390a181343220302&q=${location}&days=1&aqi=no&alerts=no`)
   .then(response => response.json())
--- other2.js
+++ other2.js
@@ -1,37 +1,40 @@
 export function outsideMethod2() {
 
-    let obj;
+    const location = 'london'
+    
+    async function fetchWeatherJSON() {
+        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e5fa685f438648b390a181343220302&q=${location}&days=1&aqi=no&alerts=no`);
+        console.log(response);
+        return await response.json();
+    }
+    fetchWeatherJSON().then(obj => {
+        // current weather data
+        console.log(obj)
+        const cloud = obj.current.cloud;
+        const condition_text = obj.current.condition.text;
+        const feelsLike_f = obj.current.feelslike_f;
+        const humidity = obj.current.humidity;
+        // 1 = yes, 0 = no
+        const is_day = obj.current.is_day;
+        const last_updated = obj.current.last_updated;
+        const precip_in = obj.current.precip_in;
+        const precip_mm = obj.current.precip_mm;
+        const temp_f = obj.current.temp_f;
+        const uv = obj.current.uv;
+        const wind_degree = obj.current.wind_degree;
+        const wind_dir = obj.current.wind_dir;
+        const wind_mph = obj.current.wind_mph;
 
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=e5fa685f438648b390a181343220302&q=${location}&days=1&aqi=no&alerts=no`)
-        .then(response => response.json())
-        .then(data => obj = data)
-        .then(data => console.log(data));
-
-    // current weather data
-    const cloud = obj.current.cloud;
-    const condition_text = obj.current.conditon.text;
-    const feelsLike_f = obj.current.feelslike_f;
-    const humidity = obj.current.humidity;
-    // 1 = yes, 0 = no
-    const is_day = obj.current.is_day;
-    const last_updated = obj.current.last_updated;
-    const precip_in = obj.current.precip_in;
-    const precip_mm = obj.current.precip_mm;
-    const temp_f = obj.current.temp_f;
-    const uv = obj.current.uv;
-    const wind_degree = obj.current.wind_degree;
-    const wind_dir = obj.current.wind_dir;
-    const wind_mph = obj.current.wind_mph;
-
-    // location data
-    const country = obj.current.location.country;
-    const lat = obj.current.location.lat;
-    const localtime = obj.current.location.localtime;
-    const localtime_epoch = obj.current.location.localtime_epoch;
-    const lon = obj.current.location.lon;
-    const name = obj.current.location.name;
-    const region = obj.current.location.region;
-    const tz_id = obj.current.location.tz_id;
+        // location data
+        const country = obj.location.country;
+        const lat = obj.location.lat;
+        const localtime = obj.location.localtime;
+        const localtime_epoch = obj.location.localtime_epoch;
+        const lon = obj.location.lon;
+        const name = obj.location.name;
+        const region = obj.location.region;
+        const tz_id = obj.location.tz_id;
+    });
 
     // forecast data
     void function setForecastDay(day) {
