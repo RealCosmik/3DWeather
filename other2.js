f77b2bb9bd66a02cdf2464a1a5bdbc93882209a0
githendumukiri <Barakaboston12>
2022-02-24 15:30:24 -0500

    syntax error & documentation of parameters

--- other2.js
+++ other2.js
@@ -3,32 +3,33 @@ export function outsideMethod2() {
     const location = 'london'
 
     async function fetchWeatherJSON() {
+
         const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e5fa685f438648b390a181343220302&q=${location}&days=1&aqi=no&alerts=no`);
         console.log(response);
         return await response.json();
     }
+
     /**
-     * @param obj
-     * @param obj.current
-     * @param obj.current.cloud
-     * @param obj.current.condition.text
-     * @param obj.current.feelslike_f
-     * @param obj.current.humidity
-     * @param obj.current.humidity
-     * @param obj.current.is_day
-     * @param obj.current.last_updated
-     * @param obj.current.precip_in
-     * @param obj.current.precip_mm
-     * @param obj.current.temp_f
-     * @param obj.current.wind_degree
-     * @param obj.current.wind_dir;
-     * @param obj.current.wind_mph;
-     * @param obj.location.country;
-     * @param obj.location.lat;
-     * @param obj.location.localtime;
-     * @param obj.location.localtime_epoch;
-     * @param obj.location.lon;
-     * @param obj.location.tz_id;
+     * @param {Object} obj - WeatherAPI.com JSON response
+     * @param {Object} obj.current - Current weather object
+     * @param {number} obj.current.cloud - Cloud cover as percentage
+     * @param {string} obj.current.condition.text - Weather condition text
+     * @param {number} obj.current.feelslike_f - Feels like temperature in fahrenheit
+     * @param {number} obj.current.humidity - Humidity as percentage
+     * @param {number} obj.current.is_day - 1 = Yes 0 = No; Whether to show day condition icon or night icon
+     * @param {string} obj.current.last_updated - Local time when the real time data was updated.
+     * @param {number} obj.current.precip_in - Precipitation amount in inches
+     * @param {number} obj.current.precip_mm - Precipitation amount in millimeters
+     * @param {number} obj.current.temp_f - Temperature in fahrenheit
+     * @param {number} obj.current.wind_degree - Wind direction in degrees
+     * @param {string} obj.current.wind_dir - Wind direction as 16 point compass. e.g.: NSW
+     * @param {number} obj.current.wind_mph - Wind speed in miles per hour
+     * @param {string} obj.location.country - Location country
+     * @param {number} obj.location.lat - Latitude in decimal degree
+     * @param {string} obj.location.localtime - Local date and time
+     * @param {number} obj.location.localtime_epoch - Local date and time in unix time
+     * @param {number} obj.location.lon - Longitude in decimal degree
+     * @param {string} obj.location.tz_id - Time zone name
      */
     fetchWeatherJSON().then(obj => {
         // current weather data
@@ -60,22 +61,22 @@ export function outsideMethod2() {
     });
 
     /**
-     * @param obj.forecast.forecastday.astro.moon_illumination
-     * @param obj.forecast.forecastday.astro.moon_phase
-     * @param obj.forecast.forecastday.astro.moonrise
-     * @param obj.forecast.forecastday.astro.moonset
-     * @param obj.forecast.forecastday.astro.sunrise;
-     * @param obj.forecast.forecastday.astro.sunset
-     * @param obj.forecast.forecastday.date
-     * @param obj.forecast.forecastday.day.avgtemp_f
-     * @param obj.forecast.forecastday.day.daily_chance_of_rain
-     * @param obj.forecast.forecastday.day.daily_chance_of_snow
-     * @param obj.forecast.forecastday.day.daily_will_it_rain
-     * @param obj.forecast.forecastday.day.daily_will_it_snow
-     * @param obj.forecast.forecastday.day.maxtemp_f
-     * @param obj.forecast.forecastday.day.maxwind_mph
-     * @param obj.forecast.forecastday.day.mintemp_f
-     * @param obj.forecast.forecastday.day.totalprecip_in;
+     * @param {number} obj.forecast.forecastday.astro.moon_illumination -    Moon illumination as %
+     * @param {string} obj.forecast.forecastday.astro.moon_phase - Moon phases. Value returned: New Moon, Waxing Crescent, First Quarter, Waxing Gibbous, Full Moon, Waning Gibbous, Last Quarter, Waning Crescent
+     * @param {string} obj.forecast.forecastday.astro.moonrise - Moonrise time
+     * @param {string} obj.forecast.forecastday.astro.moonset - Moonset time
+     * @param {string} obj.forecast.forecastday.astro.sunrise - Sunrise time
+     * @param {string} obj.forecast.forecastday.astro.sunset - Sunset time
+     * @param {string} obj.forecast.forecastday.date - Forecast date
+     * @param {number} obj.forecast.forecastday.day.avgtemp_f - Average temperature in fahrenheit for the day
+     * @param {number} obj.forecast.forecastday.day.daily_chance_of_rain - Chance of rain as percentage
+     * @param {number} obj.forecast.forecastday.day.daily_chance_of_snow - Chance of snow as percentage
+     * @param {number} obj.forecast.forecastday.day.daily_will_it_rain - 1 = Yes 0 = No; Will it will rain or not
+     * @param {number} obj.forecast.forecastday.day.daily_will_it_snow - 1 = Yes 0 = No; Will it snow or not
+     * @param {number} obj.forecast.forecastday.day.maxtemp_f - Maximum temperature in fahrenheit for the day
+     * @param {number} obj.forecast.forecastday.day.maxwind_mph - Maximum wind speed in miles per hour
+     * @param {number} obj.forecast.forecastday.day.mintemp_f - Minimum temperature in fahrenheit for the day
+     * @param {number} obj.forecast.forecastday.day.totalprecip_in - Total precipitation in inches
      *
      */
     // forecast data
