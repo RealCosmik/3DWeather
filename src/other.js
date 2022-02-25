be889d712eaa345f003b3229a9ffca7e76f26db5
githendumukiri <Barakaboston12>
2022-02-25 01:40:11 -0500

    syntax fixes

--- src/other.js
+++ src/other.js
@@ -3,11 +3,11 @@ export function outsideMethod() {
     //Creating a date variable
     outsideMethod2();
 
-    var date = localtime;
+    const date = localtime;
     //Local time in yyyy-MM-dd HH:mm format
 
     //formatting time to be seen in the console
-    var time1 = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    const time1 = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
     //logging the time in the console
     console.log(time1);
