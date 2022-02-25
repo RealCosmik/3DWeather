80f11c4462abb2f6aa13bec23c8004e992f64544
githendumukiri <Barakaboston12>
2022-02-25 01:39:19 -0500

    local time logic & syntax

--- src/other.js
+++ src/other.js
@@ -1,7 +1,10 @@
 
 export function outsideMethod() {
     //Creating a date variable
-    var date = new Date();
+    outsideMethod2();
+
+    var date = localtime;
+    //Local time in yyyy-MM-dd HH:mm format
 
     //formatting time to be seen in the console
     var time1 = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
