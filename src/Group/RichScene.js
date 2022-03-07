d6ca116c5993bc6a9983bb75d10444d19e4f6ef9
Richard Colorusso <colorussor@wit.edu>
2022-03-07 00:43:47 -0500

    Search Bar Function
    Making the search bar function. When the user inputs text and hits Enter or the search icon, it outputs to the console.
    
    Need to link the text input to have the scene update based upon the new zipcode

--- src/Group/RichScene.js
+++ src/Group/RichScene.js
@@ -5,4 +5,31 @@ export async function Initalize(scene, camera, canvas) {
     entry.RegisterOnSceneUpdate(OnSceneUpdate);
 }
 function OnSceneUpdate(deltaTime) {
+    // console.log("RichScene");
+    // //https://stackoverflow.com/questions/21491119/sending-form-text-input-to-console-log-for-testing
+    // var zipInput = document.getElementById('zip_input');
+
+    // document.querySelector('div.input').addEventListener('submit', function (e) {
+
+    //     //prevent the normal submission of the form
+    //     e.preventDefault();
+    
+    //     console.log("Zip Code Input = " + zipInput.value);
+    //     console.log("ZipValue Recieved");    
+    // });
+    //console.log("RichScene");
+    //console.log(document.getElementById('zip_input').value);
+
 }
+var zipInput = document.getElementById('zip_input');
+
+export{zipInput};
+
+//https://stackoverflow.com/questions/21491119/sending-form-text-input-to-console-log-for-testing
+document.querySelector('form.input').addEventListener('submit', function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+    console.log("ZipValue Recieved: " +zipInput.value);   
+});
