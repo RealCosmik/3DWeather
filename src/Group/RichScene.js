63905c493b3c1edf043a3c597eb5420e882f204a
Richard Colorusso <colorussor@wit.edu>
2022-03-08 19:51:37 -0500

    Documentation
    Documenting RichScene.js

--- src/Group/RichScene.js
+++ src/Group/RichScene.js
@@ -5,31 +5,24 @@ export async function Initalize(scene, camera, canvas) {
     entry.RegisterOnSceneUpdate(OnSceneUpdate);
 }
 function OnSceneUpdate(deltaTime) {
-    // console.log("RichScene");
-    // //https://stackoverflow.com/questions/21491119/sending-form-text-input-to-console-log-for-testing
-    // var zipInput = document.getElementById('zip_input');
-
-    // document.querySelector('div.input').addEventListener('submit', function (e) {
-
-    //     //prevent the normal submission of the form
-    //     e.preventDefault();
-    
-    //     console.log("Zip Code Input = " + zipInput.value);
-    //     console.log("ZipValue Recieved");    
-    // });
-    //console.log("RichScene");
-    //console.log(document.getElementById('zip_input').value);
-
+    //add code here
 }
+
+// This variable is the stored zipcode input by the user
 var zipInput = document.getElementById('zip_input');
 
+//exporting the user input for global access
 export{zipInput};
 
+//The logic below was obtained from Stackoverflow
 //https://stackoverflow.com/questions/21491119/sending-form-text-input-to-console-log-for-testing
+
+//Event listener for the search bar when the user hits "Enter" or clicks the search icon
 document.querySelector('form.input').addEventListener('submit', function (e) {
 
     //prevent the normal submission of the form
     e.preventDefault();
 
+    //Logging the output to the console
     console.log("ZipValue Recieved: " +zipInput.value);   
 });
