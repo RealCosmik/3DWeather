57697a51a1b47310a1dbfa36b8c74a9fbc5a9293
githendumukiri <Barakaboston12>
2022-04-25 01:12:19 -0400

    Grass

--- src/Group/GithenduScene.js
+++ src/Group/GithenduScene.js
@@ -8,14 +8,23 @@ import * as WeatherHelper from "../WeatherAPI";
 import {WeatherData} from "../WeatherAPI";
 
 let newWindmill;
+
+let mainCamera;
+
+let grassMaterial;
+let groundShader;
+let radius;
+let pos;
+
 const clock = new THREE.Clock();
 
 export async function Initalize(scene, camera, canvas) {
 
+    mainCamera = camera;
+
     scene.background = new THREE.Color(0x53789e);
     var groupModels = new THREE.Group();
     var groupTexts = new THREE.Group();
-    var groupSunCloud = new THREE.Group();
 
     var textLocation = await Text(scene, WeatherHelper.WeatherData.location.name);
     textLocation.position.set(-2.4, 4, 2);
@@ -39,8 +48,7 @@ export async function Initalize(scene, camera, canvas) {
     textTemperature.position.set(-2.4, 1, 2);
     textTemperature.rotateY(-300);
 
-
-    const newFloor = await Floor.CreateFloor();
+    //const newFloor = await Floor.CreateFloor();
     const newRoom = await Room.CreateRoom();
 
     newWindmill = await windmill.CreateWindmill();
@@ -48,12 +56,10 @@ export async function Initalize(scene, camera, canvas) {
     newWindmill.windmillGroup.rotateY(45);
 
     scene.add(newWindmill.windmillGroup);
-    groupModels.add(newFloor.floorModel, newRoom.roomModel);
+    groupModels.add(newRoom.roomModel);
 
     groupModels.scale.set(8, 8, 8);
     groupTexts.scale.set(8, 8, 8);
-    groupSunCloud.scale.set(8, 8, 8);
-    groupSunCloud.position.set(0, 10.5, 0);
 
     camera.far = 5000;
     camera.position.set(135.7, 56.5, 51.7);
@@ -67,7 +73,7 @@ export async function Initalize(scene, camera, canvas) {
     scene.add(groupModels, groupTexts);
 
     AddLightsToScene(scene);
-    AddGrassToScene(scene);
+    AddGrassToScene(scene,canvas);
     entry.RegisterOnSceneUpdate(Update);
 }
 
@@ -116,9 +122,42 @@ function AddLightsToScene(scene) {
      */
 }
 
-function AddGrassToScene(scene){
+function AddGrassToScene(scene, canvas){
+    //Variables for blade mesh
+    var joints = 4;
+    var bladeWidth = 0.12;
+    var bladeHeight = 8;
 
-//Get alpha map and blade texture
+//Number of vertices on ground plane side
+    var resolution = 64;
+//Distance between two ground plane vertices
+    var delta = canvas.width/resolution;
+//Radius of the sphere onto which the ground plane is bent
+    radius = 1;
+
+//The global coordinates
+//The geometry never leaves a box of width*width around (0, 0)
+//But we track where in space the camera would be globally
+    pos = new THREE.Vector2(0.01, 0.01);
+
+//Number of blades
+    var instances = 600000;
+
+    //Lighting variables for grass
+    var ambientStrength = 0.7;
+    var translucencyStrength = 1.5;
+    var specularStrength = 0.5;
+    var diffuseStrength = 1.5;
+    var shininess = 256;
+    var sunColour = new THREE.Vector3(1.0, 1.0, 1.0);
+    var specularColour = new THREE.Vector3(1.0, 1.0, 1.0);
+
+    //Grass scene
+    //Light for ground plane
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+
+    //Get alpha map and blade texture
 //These have been taken from "Realistic real-time grass rendering" by Eddie Lee, 2010
     var loader = new THREE.TextureLoader();
     loader.crossOrigin = '';
@@ -128,30 +167,324 @@ function AddGrassToScene(scene){
     noiseTexture.wrapS = THREE.RepeatWrapping;
     noiseTexture.wrapT = THREE.RepeatWrapping;
 
-//************** Grass **************
-    //Number of blades
-    var instances = 40000;
-//Variables for blade mesh
-    var joints = 4;
-    var bladeWidth = 0.12;
-    var bladeHeight = 1;
+    //************** Ground **************
+//Ground material is a modification of the existing THREE.MeshPhongMaterial rather than one from scratch
+    var groundBaseGeometry = new THREE.PlaneBufferGeometry(canvas.width, canvas.width, resolution, resolution);
+    groundBaseGeometry.lookAt(new THREE.Vector3(0,1,0));
+    groundBaseGeometry.verticesNeedUpdate = true;
 
-//Patch side length
-    var width = 120;
-//Number of vertices on ground plane side
-    var resolution = 64;
-//Distance between two ground plane vertices
-    var delta = width/resolution;
-//Radius of the sphere onto which the ground plane is bent
-    var radius = 240;
-//User movement speed
-    var speed = 3;
-//Rotation around Y axis in range [0, 2*PI]
-    var azimuth = 0.4;
-//The global coordinates
-//The geometry never leaves a box of width*width around (0, 0)
-//But we track where in space the camera would be globally
-    var pos = new THREE.Vector2(0.01, 0.01);
+    var groundGeometry = new THREE.PlaneBufferGeometry(canvas.width, canvas.width, resolution, resolution);
+    groundGeometry.setAttribute('basePosition', groundBaseGeometry.getAttribute("position"));
+    groundGeometry.lookAt(new THREE.Vector3(0,1,0));
+    groundGeometry.verticesNeedUpdate = true;
+    var groundMaterial = new THREE.MeshPhongMaterial({color: 0x000900});
+
+    var sharedPrefix = `
+uniform sampler2D noiseTexture;
+float getYPosition(vec2 p){
+	return 8.0*(2.0*texture2D(noiseTexture, p/800.0).r - 1.0);
+}
+`;
+
+    var groundVertexPrefix = sharedPrefix + ` 
+attribute vec3 basePosition;
+uniform float delta;
+uniform float posX;
+uniform float posZ;
+uniform float radius;
+uniform float width;
+
+float placeOnSphere(vec3 v){
+  float theta = acos(v.z/radius);
+  float phi = acos(v.x/(radius * sin(theta)));
+  float sV = radius * sin(theta) * sin(phi);
+  //If undefined, set to default value
+  if(sV != sV){
+    sV = v.y;
+  }
+  return sV;
+}
+
+//Get the position of the ground from the [x,z] coordinates, the sphere and the noise height field
+vec3 getPosition(vec3 pos, float epsX, float epsZ){
+  vec3 temp;
+  temp.x = pos.x + epsX;
+  temp.z = pos.z + epsZ;
+  temp.y = max(0.0, placeOnSphere(temp)) - radius;
+  temp.y += getYPosition(vec2(basePosition.x+epsX+delta*floor(posX), basePosition.z+epsZ+delta*floor(posZ)));
+  return temp;
+}
+
+//Find the normal at pos as the cross product of the central-differences in x and z directions
+vec3 getNormal(vec3 pos){
+  float eps = 1e-1;
+
+  vec3 tempP = getPosition(pos, eps, 0.0);
+  vec3 tempN = getPosition(pos, -eps, 0.0);
+  
+  vec3 slopeX = tempP - tempN;
+
+  tempP = getPosition(pos, 0.0, eps);
+  tempN = getPosition(pos, 0.0, -eps);
+
+  vec3 slopeZ = tempP - tempN;
+
+  vec3 norm = normalize(cross(slopeZ, slopeX));
+  return norm;
+}
+`;
+
+    groundMaterial.onBeforeCompile = function ( shader ) {
+        shader.uniforms.delta = { value: delta };
+        shader.uniforms.posX = { value: pos.x };
+        shader.uniforms.posZ = { value: pos.y };
+        shader.uniforms.radius = { value: radius };
+        shader.uniforms.width = { value: canvas.width };
+        shader.uniforms.noiseTexture = { value: noiseTexture };
+        shader.vertexShader = groundVertexPrefix + shader.vertexShader;
+        shader.vertexShader = shader.vertexShader.replace(
+            '#include <beginnormal_vertex>',
+            `//https://dev.to/maurobringolf/a-neat-trick-to-compute-modulo-of-negative-numbers-111e
+			vec3 pos = vec3(0);
+      pos.x = basePosition.x - mod(mod((delta*posX),delta) + delta, delta);
+      pos.z = basePosition.z - mod(mod((delta*posZ),delta) + delta, delta);
+      pos.y = max(0.0, placeOnSphere(pos)) - radius;
+      pos.y += getYPosition(vec2(basePosition.x+delta*floor(posX), basePosition.z+delta*floor(posZ)));
+      vec3 objectNormal = getNormal(pos);
+#ifdef USE_TANGENT
+      vec3 objectTangent = vec3( tangent.xyz );
+#endif`
+        );
+        shader.vertexShader = shader.vertexShader.replace(
+            '#include <begin_vertex>',
+            `vec3 transformed = vec3(pos);`
+        );
+        groundShader = shader;
+    };
+
+    var ground = new THREE.Mesh(groundGeometry, groundMaterial);
+
+    ground.position.y = .5
+    ground.scale.set(.05, .05, .05);
+
+    ground.geometry.computeVertexNormals();
+    scene.add(ground);
+
+    //************** Grass **************
+    var grassVertexSource = sharedPrefix + `
+precision mediump float;
+attribute vec3 position;
+attribute vec3 normal;
+attribute vec3 offset;
+attribute vec2 uv;
+attribute vec2 halfRootAngle;
+attribute float scale;
+attribute float index;
+uniform float time;
+
+uniform float delta;
+uniform float posX;
+uniform float posZ;
+uniform float radius;
+uniform float width;
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+varying vec2 vUv;
+varying vec3 vNormal;
+varying vec3 vPosition;
+varying float frc;
+varying float idx;
+
+const float PI = 3.1415;
+const float TWO_PI = 2.0 * PI;
+
+
+//https://www.geeks3d.com/20141201/how-to-rotate-a-vertex-by-a-quaternion-in-glsl/
+vec3 rotateVectorByQuaternion(vec3 v, vec4 q){
+  return 2.0 * cross(q.xyz, v * q.w + cross(q.xyz, v)) + v;
+}
+
+float placeOnSphere(vec3 v){
+  float theta = acos(v.z/radius);
+  float phi = acos(v.x/(radius * sin(theta)));
+  float sV = radius * sin(theta) * sin(phi);
+  //If undefined, set to default value
+  if(sV != sV){
+    sV = v.y;
+  }
+  return sV;
+}
+
+void main() {
+
+	//Vertex height in blade geometry
+	frc = position.y / float(` + bladeHeight + `);
+
+	//Scale vertices
+  vec3 vPosition = position;
+	vPosition.y *= scale;
+
+	//Invert scaling for normals
+	vNormal = normal;
+	vNormal.y /= scale;
+
+	//Rotate blade around Y axis
+  vec4 direction = vec4(0.0, halfRootAngle.x, 0.0, halfRootAngle.y);
+	vPosition = rotateVectorByQuaternion(vPosition, direction);
+	vNormal = rotateVectorByQuaternion(vNormal, direction);
+
+  //UV for texture
+  vUv = uv;
+
+	vec3 pos;
+	vec3 globalPos;
+	vec3 tile;
+
+	globalPos.x = offset.x-posX*delta;
+	globalPos.z = offset.z-posZ*delta;
+
+	tile.x = floor((globalPos.x + 0.5 * width) / width);
+	tile.z = floor((globalPos.z + 0.5 * width) / width);
+
+	pos.x = globalPos.x - tile.x * width;
+	pos.z = globalPos.z - tile.z * width;
+
+	pos.y = max(0.0, placeOnSphere(pos)) - radius;
+	pos.y += getYPosition(vec2(pos.x+delta*posX, pos.z+delta*posZ));
+	
+	//Position of the blade in the visible patch [0->1]
+  vec2 fractionalPos = 0.5 + offset.xz / width;
+  //To make it seamless, make it a multiple of 2*PI
+  fractionalPos *= TWO_PI;
+
+  //Wind is sine waves in time. 
+  float noise = sin(fractionalPos.x + time);
+  float halfAngle = noise * 0.1;
+  noise = 0.5 + 0.5 * cos(fractionalPos.y + 0.25 * time);
+  halfAngle -= noise * 0.2;
+
+	direction = normalize(vec4(sin(halfAngle), 0.0, -sin(halfAngle), cos(halfAngle)));
+
+	//Rotate blade and normals according to the wind
+  vPosition = rotateVectorByQuaternion(vPosition, direction);
+	vNormal = rotateVectorByQuaternion(vNormal, direction);
+
+	//Move vertex to global location
+	vPosition += pos;
+
+	//Index of instance for varying colour in fragment shader
+	idx = index;
+
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(vPosition, 1.0);
+
+}`;
+
+    var grassFragmentSource = `
+precision mediump float;
+
+uniform vec3 cameraPosition;
+
+//Light uniforms
+uniform float ambientStrength;
+uniform float diffuseStrength;
+uniform float specularStrength;
+uniform float translucencyStrength;
+uniform float shininess;
+uniform vec3 lightColour;
+uniform vec3 sunDirection;
+
+
+//Surface uniforms
+uniform sampler2D map;
+uniform sampler2D alphaMap;
+uniform vec3 specularColour;
+
+varying float frc;
+varying float idx;
+varying vec2 vUv;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+vec3 ACESFilm(vec3 x){
+	float a = 2.51;
+	float b = 0.03;
+	float c = 2.43;
+	float d = 0.59;
+	float e = 0.14;
+	return clamp((x*(a*x+b))/(x*(c*x+d)+e), 0.0, 1.0);
+}
+
+void main() {
+
+  //If transparent, don't draw
+  if(texture2D(alphaMap, vUv).r < 0.15){
+    discard;
+  }
+
+	vec3 normal;
+
+	//Flip normals when viewing reverse of the blade
+	if(gl_FrontFacing){
+		normal = normalize(vNormal);
+	}else{
+		normal = normalize(-vNormal);
+	}
+
+  //Get colour data from texture
+	vec3 textureColour = pow(texture2D(map, vUv).rgb, vec3(2.2));
+
+  //Add different green tones towards root
+	vec3 mixColour = idx > 0.75 ? vec3(0.07, 0.52, 0.06) : vec3(0.07, 0.43, 0.08);
+  textureColour = mix(pow(mixColour, vec3(2.2)), textureColour, frc);
+
+	vec3 lightTimesTexture = lightColour * textureColour;
+  vec3 ambient = textureColour;
+	vec3 lightDir = normalize(sunDirection);
+
+  //How much a fragment faces the light
+	float dotNormalLight = dot(normal, lightDir);
+  float diff = max(dotNormalLight, 0.0);
+
+  //Colour when lit by light
+  vec3 diffuse = diff * lightTimesTexture;
+
+  float sky = max(dot(normal, vec3(0,1,0)), 0.0);
+	vec3 skyLight = sky * vec3(0.12, 0.29, 0.55);
+
+  vec3 viewDirection = normalize(cameraPosition - vPosition);
+  vec3 halfwayDir = normalize(lightDir + viewDirection);
+  //How much a fragment directly reflects the light to the camera
+  float spec = pow(max(dot(normal, halfwayDir), 0.0), shininess);
+  
+  //Colour of light sharply reflected into the camera
+  vec3 specular = spec * specularColour * lightColour;
+
+	//https://en.wikibooks.org/wiki/GLSL_Programming/Unity/Translucent_Surfaces
+	vec3 diffuseTranslucency = vec3(0);
+	vec3 forwardTranslucency = vec3(0);
+	float dotViewLight = dot(-lightDir, viewDirection);
+	if(dotNormalLight <= 0.0){
+		diffuseTranslucency = lightTimesTexture * translucencyStrength * -dotNormalLight;
+		if(dotViewLight > 0.0){
+			forwardTranslucency = lightTimesTexture * translucencyStrength * pow(dotViewLight, 16.0);
+		}
+	}
+
+  vec3 col = 0.3 * skyLight * textureColour + ambientStrength * ambient + diffuseStrength * diffuse + specularStrength * specular + diffuseTranslucency + forwardTranslucency;
+
+  //Tonemapping
+  col = ACESFilm(col);
+
+  //Gamma correction 1.0/2.2 = 0.4545...
+	col = pow(col, vec3(0.4545));
+
+	//Add a shadow towards root
+	col = mix(vec3(0.0, 0.2, 0.0), col, frc);
+
+  gl_FragColor = vec4(col, 1.0);
+}`;
 
 //Define base geometry that will be instanced. We use a plane for an individual blade of grass
     var grassBaseGeometry = new THREE.PlaneBufferGeometry(bladeWidth, bladeHeight, 1, joints);
@@ -219,7 +552,7 @@ function AddGrassToScene(scene){
     var baseMaterial = new THREE.MeshNormalMaterial({side: THREE.DoubleSide});
     var baseBlade = new THREE.Mesh(grassBaseGeometry, baseMaterial);
 //Show grass base geometry
-scene.add(baseBlade);
+//scene.add(baseBlade);
 
     var instancedGeometry = new THREE.InstancedBufferGeometry();
 
@@ -240,8 +573,8 @@ scene.add(baseBlade);
         indices.push(i/instances);
 
         //Offset of the roots
-        x = Math.random() * width - width/2;
-        z = Math.random() * width - width/2;
+        x = Math.random() * canvas.width - canvas.width/2;
+        z = Math.random() * canvas.width - canvas.width/2;
         y = 0;
         offsets.push(x, y, z);
 
@@ -268,39 +601,42 @@ scene.add(baseBlade);
     instancedGeometry.setAttribute( 'index', indexAttribute);
 
 //Define the material, specifying attributes, uniforms, shaders etc.
-    var grassMaterial = new THREE.RawShaderMaterial( {
+    grassMaterial = new THREE.RawShaderMaterial( {
         uniforms: {
             time: {type: 'float', value: 0},
             delta: {type: 'float', value: delta },
             posX: {type: 'float', value: pos.x },
             posZ: {type: 'float', value: pos.y },
             radius: {type: 'float', value: radius },
-            width: {type: 'float', value: width },
+            width: {type: 'float', value: canvas.width },
             map: { value: grassTexture},
             alphaMap: { value: alphaMap},
             noiseTexture: { value: noiseTexture},
-            //sunDirection: {type: 'vec3', value: new THREE.Vector3(Math.sin(azimuth), Math.sin(elevation), -Math.cos(azimuth))},
-            //cameraPosition: {type: 'vec3', value: camera.position},
-            //ambientStrength: {type: 'float', value: ambientStrength},
-            //translucencyStrength: {type: 'float', value: translucencyStrength},
-            //diffuseStrength: {type: 'float', value: diffuseStrength},
-            //specularStrength: {type: 'float', value: specularStrength},
-            //shininess: {type: 'float', value: shininess},
-            //lightColour: {type: 'vec3', value: sunColour},
-            //specularColour: {type: 'vec3', value: specularColour},
+            ambientStrength: {type: 'float', value: ambientStrength},
+            translucencyStrength: {type: 'float', value: translucencyStrength},
+            diffuseStrength: {type: 'float', value: diffuseStrength},
+            specularStrength: {type: 'float', value: specularStrength},
+            shininess: {type: 'float', value: shininess},
+            lightColour: {type: 'vec3', value: sunColour},
+            specularColour: {type: 'vec3', value: specularColour},
         },
-        //vertexShader: grassVertexSource,
-        //fragmentShader: grassFragmentSource,
+        vertexShader: grassVertexSource,
+        fragmentShader: grassFragmentSource,
         side: THREE.DoubleSide
     } );
 
     var grass = new THREE.Mesh(instancedGeometry, grassMaterial);
+    grass.position.y = 0.5;
+    grass.scale.set(.05, .05, .05);
+
     scene.add(grass);
 }
 
-
 function Update(deltaTime) {
 
-    if (WeatherData.current.wind_mph)
+    if (WeatherData.current.wind_mph){
+
         newWindmill.rotateWindmill(WeatherData.current.wind_mph * deltaTime);
+        grassMaterial.uniforms.time.value += deltaTime * WeatherData.current.wind_mph ;
+    }
 }
