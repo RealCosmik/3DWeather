5237aca3388edd78bfea890f00a85200eeb197a2
Justin Nguyen <nguyenj32@wit.edu>
2022-04-26 22:27:39 -0400

    Removed Controls From Water.js

--- src/SceneObjects/Water.js
+++ src/SceneObjects/Water.js
@@ -16,22 +16,6 @@ export class Water {
     static async CreateWater() {
         const newWater = new Water();
         newWater.groupModel = new THREE.Group();
-        const gui = new dat.GUI({width: 340});
-        const debugObject = {};
-
-        debugObject.depthColor = "#186691";
-        debugObject.surfaceColor = "#9bd8ff";
-
-        gui.addColor(debugObject, "depthColor").onChange(() => {
-            newWater.waterMaterial.uniforms.uDepthColor.value.set(
-                debugObject.depthColor
-            );
-        });
-        gui.addColor(debugObject, "surfaceColor").onChange(() => {
-            newWater.waterMaterial.uniforms.uSurfaceColor.value.set(
-                debugObject.surfaceColor
-            );
-        });
 
         const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512);
 
@@ -50,74 +34,12 @@ export class Water {
                 uSmallWavesSpeed: {value: 0.2},
                 uSmallIterations: {value: 4},
 
-                uDepthColor: {value: new THREE.Color(debugObject.depthColor)},
-                uSurfaceColor: {value: new THREE.Color(debugObject.surfaceColor)},
+                uDepthColor: {value: new THREE.Color("#186691")},
+                uSurfaceColor: {value: new THREE.Color("#9bd8ff")},
                 uColorOffset: {value: 0.08},
                 uColorMultiplier: {value: 5},
             },
         });
-        gui
-            .add(newWater.waterMaterial.uniforms.uBigWavesElevation, "value")
-            .min(0)
-            .max(1)
-            .step(0.001)
-            .name("uBigWavesElevation");
-        gui
-            .add(newWater.waterMaterial.uniforms.uBigWavesFrequency.value, "x")
-            .min(0)
-            .max(10)
-            .step(0.001)
-            .name("uBigWavesFrequencyX");
-        gui
-            .add(newWater.waterMaterial.uniforms.uBigWavesFrequency.value, "y")
-            .min(0)
-            .max(10)
-            .step(0.001)
-            .name("uBigWavesFrequencyY");
-        gui
-            .add(newWater.waterMaterial.uniforms.uBigWavesSpeed, "value")
-            .min(0)
-            .max(4)
-            .step(0.001)
-            .name("uBigWavesSpeed");
-
-        gui
-            .add(newWater.waterMaterial.uniforms.uSmallWavesElevation, "value")
-            .min(0)
-            .max(1)
-            .step(0.001)
-            .name("uSmallWavesElevation");
-        gui
-            .add(newWater.waterMaterial.uniforms.uSmallWavesFrequency, "value")
-            .min(0)
-            .max(30)
-            .step(0.001)
-            .name("uSmallWavesFrequency");
-        gui
-            .add(newWater.waterMaterial.uniforms.uSmallWavesSpeed, "value")
-            .min(0)
-            .max(4)
-            .step(0.001)
-            .name("uSmallWavesSpeed");
-        gui
-            .add(newWater.waterMaterial.uniforms.uSmallIterations, "value")
-            .min(0)
-            .max(5)
-            .step(1)
-            .name("uSmallIterations");
-
-        gui
-            .add(newWater.waterMaterial.uniforms.uColorOffset, "value")
-            .min(0)
-            .max(1)
-            .step(0.001)
-            .name("uColorOffset");
-        gui
-            .add(newWater.waterMaterial.uniforms.uColorMultiplier, "value")
-            .min(0)
-            .max(10)
-            .step(0.001)
-            .name("uColorMultiplier");
 
         // Mesh
         const water = new THREE.Mesh(waterGeometry, newWater.waterMaterial);
@@ -127,7 +49,6 @@ export class Water {
 
         const wallNorth = new THREE.Mesh(
             new THREE.BoxGeometry(2, 0.25, 0.1),
-            //#FFFFFF
             new THREE.MeshStandardMaterial({color: "White"})
         );
         wallNorth.position.set(1, 0, 0);
